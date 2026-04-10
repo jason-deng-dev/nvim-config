4556a72b4cbdca5b3ddfcd4a1590818ba07ea6ef
jd <jason13.deng@gmail.com>
2026-04-10 16:11:08 +0900

    feat: change escape mapping from jj to jk

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode" })
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode" })
 vim.keymap.set("n", "<leader>r", function()
   vim.cmd("w")
   local dir = vim.fn.expand("%:p:h")
